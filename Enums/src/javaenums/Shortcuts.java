2fe3632429646bdb1d247bcb0649d5558f35da31
NandanaH <Nandana.H@ust.com>
2023-03-18 14:54:10 +0530

    t

--- Enums/src/javaenums/Shortcuts.java
+++ Enums/src/javaenums/Shortcuts.java
@@ -1,9 +1,13 @@
 package javaenums;
 
+import java.util.HashMap;
+
 public class Shortcuts {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		HashMap map = new HashMap();
+		
 
 	}
 
--- javarecords/src/javarecords/TestStudent.java
+++ javarecords/src/javarecords/TestStudent.java
@@ -3,7 +3,13 @@ package javarecords;
 public class TestStudent {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Person john = new Person("john",25);
+		Person sarah = new Person("sarah",30);
+		
+		System.out.println(john.name);
+		System.out.println(sarah.name);
+		
 
 	}
 
